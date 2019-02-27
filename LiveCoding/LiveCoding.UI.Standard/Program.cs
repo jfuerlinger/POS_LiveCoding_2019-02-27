3f1c2fc4d8c3508479ec78d9541813aa7c93c440
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2019-02-27 21:12:02 +0100

    Add Settings.

---
+++ LiveCoding/LiveCoding.UI.Standard/Program.cs
@@ -0,0 +1,13 @@
+﻿using LiveCoding.UI.Standard.Properties;
+using System;
+
+namespace LiveCoding.UI.Standard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Settings.Default.HeaderText);
+        }
+    }
+}
---
+++ LiveCoding/LiveCoding.UI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Utils;
+
+namespace LiveCoding.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string fileName = MyFile.GetFullNameInApplicationTree("Races.xml");
+
+            Console.WriteLine("----- H E A D E R -----");
+
+
+
+        }
+    }
+}
